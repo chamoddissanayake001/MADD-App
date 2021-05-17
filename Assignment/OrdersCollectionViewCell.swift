b1378efd94f4f074faf6136004adf0f2e1dc396b
user173744 <user173744@AS419.local>
2021-05-18 02:02:23 +0800

    my Orders network integration

--- Assignment/OrdersCollectionViewCell.swift
+++ Assignment/OrdersCollectionViewCell.swift
@@ -8,10 +8,41 @@
 
 import UIKit
 
+extension UIImageView {
+    func downloadFrom1(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloadFrom1(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadFrom(from: url, contentMode: mode)
+    }
+}
+
+
 class OrdersCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var orderImageView: UIImageView!
     @IBOutlet weak var orderTitle: UILabel!
     @IBOutlet weak var orderPrice: UILabel!
     @IBOutlet weak var orderDate: UILabel!
+    
+    func setup(with myProduct: MyOrders){
+        
+        orderTitle.text = myProduct.title
+        orderPrice.text = myProduct.price
+        orderDate.text = myProduct.orderedDate
+        orderImageView.downloadFrom1(from:myProduct.image)
+        
+    }
 }
--- Assignment/views/OrdersViewController.swift
+++ Assignment/views/OrdersViewController.swift
@@ -8,28 +8,57 @@
 
 import UIKit
 
+struct MyOrders: Decodable{
+    let title: String
+    let price: String
+    let image: String
+    let orderedDate: String
+}
+
 class OrdersViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    var myOrders = [MyOrders]()
+       
+    @IBOutlet weak var orderCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            let url = URL(string: "https://ac9d004b9511.ngrok.io/orders")
+            
+            URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                if error == nil{
+                    do{
+                        self.myOrders = try JSONDecoder().decode([MyOrders].self, from: data!)
+                    }catch{
+                        print("Parse Error")
+                    }
+                    DispatchQueue.main.async {
+                        self.orderCollectionView.reloadData()
+                    }
+                }
+            }.resume()
+           // Do any additional setup after loading the view.
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return orders.count
+        return myOrders.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OrdersCollectionViewCell", for: indexPath) as! OrdersCollectionViewCell
+//        cell.orderImageView.image = myOrders[indexPath.row].image
+//        cell.orderTitle.text = myOrders[indexPath.row].title
+//        cell.orderPrice.text = myOrders[indexPath.row].price
+//        cell.orderDate.text = myOrders[indexPath.row].orderedDate
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OrdersCollectionViewCell", for: indexPath) as! OrdersCollectionViewCell
-        cell.orderImageView.image = orders[indexPath.row].image
-        cell.orderTitle.text = orders[indexPath.row].title
-        cell.orderPrice.text = orders[indexPath.row].price
-        cell.orderDate.text = orders[indexPath.row].orderedDate
+        cell.setup(with: myOrders[indexPath.row])
+        
         return cell
     }
     
     
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+   
     
 
    
