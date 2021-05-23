170f56a262f328d745a8ebe491ea825c9b74aebf
user164919 <user164919@MU041.local>
2021-05-23 13:05:11 +0530

    mofifications

--- Assignment/views/LoginViewController.swift
+++ Assignment/views/LoginViewController.swift
@@ -50,9 +50,79 @@ class LoginViewController: UIViewController {
     }
     
     
+
+    struct TempJsonUser: Encodable{
+        let username: String
+        let password: String
+    }
+
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
 //        go_to_tab_view
         let login_btn_temp : UIButton = sender as! UIButton
+        
+        
+//       https://www.appsdeveloperblog.com/http-post-request-example-in-swift/
+        
+//        let url = URL(string: "http://13.235.27.22:5000/users/validate")
+//        guard let requestUrl = url else { fatalError() }
+//        var request = URLRequest(url: requestUrl)
+//        request.httpMethod = "POST"
+//        // Set HTTP Request Header
+//        request.setValue("application/json", forHTTPHeaderField: "Accept")
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        let newTodoItem = TempJsonUser(username: txt_username.text!, password: txt_password.text!)
+//        let jsonData = try JSONEncoder().encode(newTodoItem)
+//        request.httpBody = jsonData
+//            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+//
+//                if let error = error {
+//                    print("Error took place \(error)")
+//                    return
+//                }
+//                guard let data = data else {return}
+//                do{
+//                    let todoItemModel = try JSONDecoder().decode(TempJsonUser.self, from: data)
+//                    print("Response data:\n \(todoItemModel)")
+//                    print("todoItemModel Title: \(todoItemModel.title)")
+//                    print("todoItemModel id: \(todoItemModel.id ?? 0)")
+//                }catch let jsonErr{
+//                    print(jsonErr)
+//               }
+//
+//        }
+//        task.resume()
+         
+        
+//        // prepare json data
+//        let json: [String: Any] = ["username": txt_username.text!,
+//                                   "password": txt_password.text!]
+//
+//        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+//
+//        // create post request
+//        let url = URL(string: "http://13.235.27.22:5000/users/validate")!
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//
+//        // insert json data to the request
+//        request.httpBody = jsonData
+//
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            guard let data = data, error == nil else {
+//                print(error?.localizedDescription ?? "No data")
+//                return
+//            }
+//            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+//            if let responseJSON = responseJSON as? [String: Any] {
+//                print(responseJSON)
+//            }
+//        }
+//
+//        task.resume()
+        
+        
+        
         
         switch login_btn_temp {
             case btn_login:
